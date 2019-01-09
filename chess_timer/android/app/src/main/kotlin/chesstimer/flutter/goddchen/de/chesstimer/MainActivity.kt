785b2d397b98922d2456c6f611e9b53d0dedf35f
Martin Liersch <martin@formigas.de>
2019-01-09 15:39:16 +0100

    initial project import

---
+++ chess_timer/android/app/src/main/kotlin/chesstimer/flutter/goddchen/de/chesstimer/MainActivity.kt
@@ -0,0 +1,11 @@
+package chesstimer.flutter.goddchen.de.chesstimer
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
