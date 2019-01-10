be010acce207229051994e5497c41c1283b58e92
Goddchen <goddchen@gmail.com>
2019-01-10 21:02:59 +0100

    add some icons

--- chess_timer/lib/middle_area.dart
+++ chess_timer/lib/middle_area.dart
@@ -3,9 +3,36 @@ import 'package:flutter/material.dart';
 class MiddleArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      height: 80,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        InkWell(
+          child: SizedBox.fromSize(
+            size: Size.square(48),
+            child: Icon(Icons.photo_album),
+          ),
+          onTap: () {},
+        ),
+        InkWell(
+          child: SizedBox.fromSize(
+            size: Size.square(48),
+            child: Icon(Icons.photo_album),
+          ),
+          onTap: () {},
+        ),InkWell(
+          child: SizedBox.fromSize(
+            size: Size.square(48),
+            child: Icon(Icons.photo_album),
+          ),
+          onTap: () {},
+        ),InkWell(
+          child: SizedBox.fromSize(
+            size: Size.square(48),
+            child: Icon(Icons.photo_album),
+          ),
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
