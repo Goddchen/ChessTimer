8d222e2a3e5eaf372f9591e360f9b6eb78f41347
Christopher Hauß <christopher@formigas.de>
2019-01-10 17:25:55 +0100

    added stupid colors playersarea and middlearea somehow layouted

---
+++ chess_timer/lib/middle_area.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MiddleArea extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      height: 80,
+    );
+  }
+}
---
+++ chess_timer/lib/players_area.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlayersArea extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _PlayerState();
+}
+
+class _PlayerState extends State<PlayersArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red, height: 300);
+  }
+}
