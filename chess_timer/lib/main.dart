643bb665cb34781bddc5e66cd1b89cd189b74725
chauss <christopher_hauss@gmx.de>
2019-01-10 20:48:23 +0100

    little buggy timer switching implemented

--- chess_timer/lib/main.dart
+++ chess_timer/lib/main.dart
@@ -5,10 +5,38 @@ import 'dart:math';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+    State<StatefulWidget> createState() => _ChessTimerState();
+}
+
+class _ChessTimerState extends State<MyApp> {
+  int _playerAtTurn = 0;
+  var _playersTime = [0, 10, 10];  // 0 is for pause
+  
+  void firstPlayerStarted(int remainingTime) {
+    if (_playerAtTurn != 1) {
+      debugPrint("First Player started");
+      setState(() {
+        _playersTime[_playerAtTurn] = remainingTime;
+        _playerAtTurn = 1;
+      });
+    }
+  }
+
+  void secondPlayerStarted(int remainingTime) {
+    if (_playerAtTurn != 2) {
+      debugPrint("Second Player started");
+      setState(() {
+        _playersTime[_playerAtTurn] = remainingTime;
+        _playerAtTurn = 2;
+      });
+    }
+  }
+
+  @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
         title: 'Chess Timer',
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -24,7 +52,9 @@ class MyApp extends StatelessWidget {
                       child: Container(
                         margin: EdgeInsets.all(8),
                         child: PlayersArea(
-                          f: () => debugPrint("Clicked"),
+                          isActive: _playerAtTurn == 1,
+                          playersTimeStart: _playersTime[1],
+                          clickedCallback: firstPlayerStarted,
                         ),
                       ),
                     ),
@@ -34,7 +64,9 @@ class MyApp extends StatelessWidget {
                     child: Container(
                       margin: EdgeInsets.all(8),
                       child: PlayersArea(
-                        f: () => debugPrint("Clicked"),
+                        isActive: _playerAtTurn == 2,
+                        playersTimeStart: _playersTime[2],
+                        clickedCallback: secondPlayerStarted,
                       ),
                     ),
                   )
@@ -43,5 +75,5 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ));
-  }
+    }
 }
--- chess_timer/lib/players_area.dart
+++ chess_timer/lib/players_area.dart
@@ -2,42 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:quiver/async.dart';
 
 class PlayersArea extends StatefulWidget {
-  final Function f;
-  const PlayersArea({Key key, this.f}) : super(key: key);
+  final Function clickedCallback;
+  final bool _isActive;
+  final int _playersTimeStart;  // in seconds
+
+  const PlayersArea({Key key, this.clickedCallback, isActive, playersTimeStart}) :
+     _isActive = isActive,  _playersTimeStart = playersTimeStart, super(key: key);
 
   @override
   State<StatefulWidget> createState() => _PlayerState();
 }
 
 class _PlayerState extends State<PlayersArea> {
-  int _timerSeconds;
-  bool _active;
+  int _playersTime;  // in seconds
+  CountdownTimer timer;
 
-  void activate(int time) {
+  void _activateTimer() {
     setState(() {
-      _timerSeconds = time;
-      _active = true;
+      _playersTime = widget._playersTimeStart;
     });
-    CountdownTimer(Duration(seconds: _timerSeconds, milliseconds: 500),
-            Duration(seconds: 1))
-        .listen((timer) {
+    timer = CountdownTimer(Duration(seconds: _playersTime, milliseconds: 500),
+            Duration(seconds: 1));
+    timer.listen((timer) {
       setState(() {
-        _timerSeconds = timer.remaining.inSeconds;
+        _playersTime = timer.remaining.inSeconds;
       });
     });
   }
 
+  void _deactivateTimer() {
+    if (timer != null) {
+      timer.cancel();
+    }
+  }
+
+  int _getRemainingTime() {
+    if (timer != null) {
+      return timer.remaining.inSeconds;
+    } else if (_playersTime != null) {
+      return _playersTime;
+    }
+    return widget._playersTimeStart;
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (widget._isActive) {
+      _activateTimer();
+    } else {
+      _deactivateTimer();
+    }
+
     return Container(
       constraints: BoxConstraints.expand(),
       child: MaterialButton(
-        color: Colors.grey,
+        color: widget._isActive ? Colors.grey : Colors.lightBlue,
         onPressed: () {
-          activate(10);
-          widget.f();
+          widget.clickedCallback(_getRemainingTime());
         },
-        child: Text(_timerSeconds.toString()),
+        child: Text(_playersTime.toString()),
       ),
     );
   }
