29ebc0f36ae184b97fd3e41fd496e964fd6f8be4
Christopher Hauß <christopher@formigas.de>
2020-01-25 18:49:15 +0100

    player is now serializable

--- chess_timer/lib/model/player.dart
+++ chess_timer/lib/model/player.dart
@@ -4,13 +4,15 @@ enum PLAYER_ID { ONE, TWO }
 
 class Player {
   final Stopwatch _stopwatch = Stopwatch();
+  final int _stopwatchOffset;
 
-  int turnTimeLeft;
-  int numberOfTurns = 0;
   PLAYER_ID id;
+  int turnTimeLeft;
+  int numberOfTurns;
 
-  Player({int startTimeSeconds, this.id})
-      : this.turnTimeLeft = startTimeSeconds;
+  Player(
+      {this.id, this.turnTimeLeft, this.numberOfTurns = 0, secondsPlayed = 0})
+      : _stopwatchOffset = secondsPlayed;
 
   void startTimer() {
     _stopwatch.start();
@@ -26,7 +28,7 @@ class Player {
   }
 
   int get totalTimeSeconds {
-    return _stopwatch.elapsed.inSeconds;
+    return _stopwatch.elapsed.inSeconds + _stopwatchOffset;
   }
 
   bool get timerIsRunning {
@@ -47,10 +49,29 @@ class Player {
   }
 
   @override
-  String toString() => jsonEncode({
-        'PlayerID': id,
+  String toString() => toJson().toString();
+
+  Map toJson() => {
+        'player_id': jsonEncode(id.toString()),
         'turnTimeLeft': turnTimeLeft,
         'isActivePlayer': timerIsRunning,
         'numberOfTurns': numberOfTurns,
-      });
+        'totalTimeSeconds': totalTimeSeconds,
+      };
+
+  Player fromJson(json) => Player(
+        id: _getPlayerIDFromString(json['player_id']),
+        turnTimeLeft: json['turnTimeLeft'],
+        numberOfTurns: json['numberOfTurns'],
+        secondsPlayed: json['totalTimeSeconds'],
+      );
+
+  PLAYER_ID _getPlayerIDFromString(String idAsString) {
+    for (PLAYER_ID element in PLAYER_ID.values) {
+      if (element.toString() == idAsString) {
+        return element;
+      }
+    }
+    return null;
+  }
 }
