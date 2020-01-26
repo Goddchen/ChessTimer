6250a91eede83a6d173cec51eb336d807ac219ad
Christopher Hauß <christopher@formigas.de>
2020-01-26 16:17:54 +0100

    improved colors

--- chess_timer/lib/common/app_colors.dart
+++ chess_timer/lib/common/app_colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/rendering.dart';
 
 class AppColors {
   static const Color homeBackground = Color.fromARGB(255, 140, 190, 214);
-  static const Color player_area_active = Color.fromARGB(255, 220, 205, 120);
-  static const Color player_area_inactive = Color.fromARGB(255, 0, 124, 223);
+  static const Color player_area_active = Color.fromARGB(255, 215, 136, 72);
+  static const Color player_area_inactive = Color.fromARGB(255, 40, 119, 183);
 }
